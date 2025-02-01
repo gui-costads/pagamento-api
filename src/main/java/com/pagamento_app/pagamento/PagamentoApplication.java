d2d84da91df86e1905e9871beb4842df318afeca
guilherme costa <guilhermecosta.ds1@gmail.com>
2025-02-01 14:46:35 -0300

    arquivos iniciais

---
+++ src/main/java/com/pagamento_app/pagamento/PagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.pagamento_app.pagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagamentoApplication.class, args);
+	}
+
+}
